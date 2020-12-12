73e540d71ea00993fcb87b1d7636d24f49be4a4b
error101notfound <60568020+error101notfound@users.noreply.github.com>
2020-12-12 18:19:48 +0530

    Create Bot.py

---
+++ Bot.py
@@ -0,0 +1,20 @@
+from instapy import Instapy
+
+session =InstaPy(username="_freelancer.work_", password="freework.11")
+session. login()
+
+#following section
+
+session.set_do_follow=True, percentage-90, times=1)
+
+#Comment section
+
+session.set_do_coment(True, percentage-90) session.set_comments([u'nice one @{}!'])
+ 
+#like section
+
+session.like_by_tags(['freelancing', 'needwebsite', 'workfromhome'] ,amount=100)
+
+sension.set_dont_like(("naked", "nafw", "sex")
+
+session.end()
